V5-Client/V5Loader

internal: provide DrawContext instance

src/main/kotlin/com/chattriggers/ctjs/internal/engine/CTEvents.kt
@@ -19,6 +19,7 @@ import com.mojang.brigadier.CommandDispatcher
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
 import net.fabricmc.fabric.api.event.Event
 import net.fabricmc.fabric.api.event.EventFactory
+import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.gui.Drawable
 import net.minecraft.client.gui.screen.Screen
 import net.minecraft.client.util.math.MatrixStack
@@ -47,7 +48,7 @@ internal object CTEvents {
     }
 
     fun interface RenderOverlayCallback {
-        fun render(matrixStack: MatrixStack, partialTicks: Float)
+        fun render(context: DrawContext, matrixStack: MatrixStack, partialTicks: Float)
     }
 
     fun interface PacketReceivedCallback {
@@ -85,8 +86,8 @@ internal object CTEvents {
 
     @JvmField
     val RENDER_OVERLAY = make<RenderOverlayCallback> { listeners ->
-        RenderOverlayCallback { stack, partialTicks ->
-            listeners.forEach { it.render(stack, partialTicks) }
+        RenderOverlayCallback { ctx, stack, partialTicks ->
+            listeners.forEach { it.render(ctx, stack, partialTicks) }
         }
     }
 

src/main/kotlin/com/chattriggers/ctjs/internal/engine/module/ModuleUpdater.kt
@@ -26,7 +26,7 @@ object ModuleUpdater : Initializer {
     override fun init() {
         ClientPlayConnectionEvents.JOIN.register { _, _, _ -> shouldReportChangelog = true }
 
-        CTEvents.RENDER_OVERLAY.register { _, _ ->
+        CTEvents.RENDER_OVERLAY.register { _, _, _ ->
             if (shouldReportChangelog) {
                 changelogs.forEach(::reportChangelog)
                 changelogs.clear()

src/main/kotlin/com/chattriggers/ctjs/internal/listeners/ClientListener.kt
@@ -124,9 +124,9 @@ object ClientListener : Initializer {
             TriggerType.STEP.triggerAll()
         }
 
-        CTEvents.RENDER_OVERLAY.register { stack, partialTicks ->
+        CTEvents.RENDER_OVERLAY.register { ctx, stack, partialTicks ->
             Renderer.withMatrix(stack, partialTicks) {
-                TriggerType.RENDER_OVERLAY.triggerAll()
+                TriggerType.RENDER_OVERLAY.triggerAll(ctx)
             }
         }
 

Zephy1/ctjs

src/main/kotlin/com/chattriggers/ctjs/internal/engine/CTEvents.kt
@@ -7,6 +7,7 @@ import com.mojang.brigadier.CommandDispatcher
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
 import net.fabricmc.fabric.api.event.Event
 import net.fabricmc.fabric.api.event.EventFactory
+import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.gui.Drawable
 import net.minecraft.client.gui.screen.Screen
 import net.minecraft.client.util.math.MatrixStack
@@ -35,7 +36,7 @@ internal object CTEvents {
     }
 
     fun interface RenderHudOverlayCallback {
-        fun render(matrixStack: MatrixStack, partialTicks: Float)
+        fun render(context: DrawContext, matrixStack: MatrixStack, partialTicks: Float)
     }
 
     fun interface PacketReceivedCallback {
@@ -73,8 +74,8 @@ internal object CTEvents {
 
     @JvmField
     val RENDER_HUD_OVERLAY = make<RenderHudOverlayCallback> { listeners ->
-        RenderHudOverlayCallback { stack, partialTicks ->
-            listeners.forEach { it.render(stack, partialTicks) }
+        RenderHudOverlayCallback { ctx, stack, partialTicks ->
+            listeners.forEach { it.render(ctx, stack, partialTicks) }
         }
     }
 

src/main/kotlin/com/chattriggers/ctjs/internal/engine/module/ModuleUpdater.kt
@@ -26,7 +26,7 @@ object ModuleUpdater : Initializer {
     override fun init() {
         ClientPlayConnectionEvents.JOIN.register { _, _, _ -> shouldReportChangelog = true }
 
-        CTEvents.RENDER_HUD_OVERLAY.register { _, _ ->
+        CTEvents.RENDER_HUD_OVERLAY.register { _, _, _ ->
             if (shouldReportChangelog) {
                 changelogs.forEach(::reportChangelog)
                 changelogs.clear()

src/main/kotlin/com/chattriggers/ctjs/internal/listeners/ClientListener.kt
@@ -72,9 +72,7 @@ object ClientListener : Initializer {
                     if (it.delay-- <= 0) {
                         UMinecraft.getMinecraft().submit(it.callback)
                         true
-                    } else {
-                        false
-                    }
+                    } else false
                 }
             }
 
@@ -194,9 +192,9 @@ object ClientListener : Initializer {
             TriggerType.STEP.triggerAll()
         }
 
-        CTEvents.RENDER_HUD_OVERLAY.register { stack, partialTicks ->
+        CTEvents.RENDER_HUD_OVERLAY.register { ctx, stack, partialTicks ->
             GUIRenderer.withMatrix(UMatrixStack(stack).toMC(), partialTicks) {
-                TriggerType.RENDER_HUD_OVERLAY.triggerAll()
+                TriggerType.RENDER_HUD_OVERLAY.triggerAll(ctx)
             }
         }
 