V5-Client/V5Loader

WorldListener: add custom events as a fix

src/main/kotlin/com/chattriggers/ctjs/internal/listeners/WorldListener.kt
@@ -1,33 +1,34 @@
 package com.chattriggers.ctjs.internal.listeners
 
+import com.chattriggers.ctjs.MCBlockPos
 import com.chattriggers.ctjs.api.render.Renderer
 import com.chattriggers.ctjs.api.triggers.CancellableEvent
 import com.chattriggers.ctjs.api.triggers.TriggerType
-import com.chattriggers.ctjs.internal.utils.Initializer
-//import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents
+import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.util.math.BlockPos
 
-object WorldListener : Initializer {
-    override fun init() {
-        // fixme: whenever fabric implements this or figure out what's the new api
-//        WorldRenderEvents.BLOCK_OUTLINE.register { _, ctx ->
-//            val event = CancellableEvent()
-//            TriggerType.BLOCK_HIGHLIGHT.triggerAll(BlockPos(ctx.pos), event)
-//            !event.isCancelled()
-//        }
-//
-//        WorldRenderEvents.START.register { ctx ->
-//            val deltaTicks = ctx.tickCounter().dynamicDeltaTicks
-//            Renderer.withMatrix(ctx.matrixStack(), deltaTicks) {
-//                TriggerType.PRE_RENDER_WORLD.triggerAll(deltaTicks)
-//            }
-//        }
-//
-//        WorldRenderEvents.LAST.register { ctx ->
-//            val deltaTicks = ctx.tickCounter().dynamicDeltaTicks
-//            Renderer.withMatrix(ctx.matrixStack(), deltaTicks) {
-//                TriggerType.POST_RENDER_WORLD.triggerAll(deltaTicks)
-//            }
-//        }
+object WorldListener {
+    var matrixStack: MatrixStack? = null
+    private var deltaTicks: Float = 1f
+
+    fun triggerBlockOutline(bp: MCBlockPos): Boolean {
+        val event = CancellableEvent()
+        TriggerType.BLOCK_HIGHLIGHT.triggerAll(BlockPos(bp), event)
+        return event.isCanceled()
+    }
+
+    fun triggerRenderStart(ticks: Float) {
+        deltaTicks = ticks
+        if (matrixStack == null) return
+        Renderer.withMatrix(matrixStack, ticks) {
+            TriggerType.PRE_RENDER_WORLD.triggerAll(ticks)
+        }
+    }
+
+    fun triggerRenderLast() {
+        if (matrixStack == null) return
+        Renderer.withMatrix(matrixStack, deltaTicks) {
+            TriggerType.POST_RENDER_WORLD.triggerAll(deltaTicks)
+        }
     }
 }

Zephy1/ctjs

src/main/kotlin/com/chattriggers/ctjs/internal/listeners/WorldListener.kt
@@ -3,36 +3,58 @@ package com.chattriggers.ctjs.internal.listeners
 import com.chattriggers.ctjs.api.render.GUIRenderer
 import com.chattriggers.ctjs.api.triggers.CancellableEvent
 import com.chattriggers.ctjs.api.triggers.TriggerType
-import com.chattriggers.ctjs.internal.utils.Initializer
-//import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents
 import net.minecraft.util.math.BlockPos
 
-object WorldListener : Initializer {
-    override fun init() {
-        // fixme: whenever fabric implements this or figure out what's the new api
-//        WorldRenderEvents.BLOCK_OUTLINE.register { _, ctx ->
-//            val event = CancellableEvent()
-//            //#if MC<=12108
-//            //$$TriggerType.RENDER_BLOCK_HIGHLIGHT.triggerAll(BlockPos(ctx.blockPos()), event)
-//            //#else
-//            TriggerType.RENDER_BLOCK_HIGHLIGHT.triggerAll(BlockPos(ctx.pos()), event)
-//            //#endif
-//
-//            !event.isCancelled()
-//        }
-//
-//        WorldRenderEvents.START.register { ctx ->
-//            val deltaTicks = ctx.tickCounter().dynamicDeltaTicks
-//            GUIRenderer.withMatrix(ctx.matrixStack(), deltaTicks) {
-//                TriggerType.PRE_RENDER_WORLD.triggerAll(deltaTicks)
-//            }
-//        }
-//
-//        WorldRenderEvents.LAST.register { ctx ->
-//            val deltaTicks = ctx.tickCounter().dynamicDeltaTicks
-//            GUIRenderer.withMatrix(ctx.matrixStack(), deltaTicks) {
-//                TriggerType.POST_RENDER_WORLD.triggerAll(deltaTicks)
-//            }
-//        }
+//#if MC<=12108
+//$$import com.chattriggers.ctjs.internal.utils.Initializer
+//$$import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents
+//$$object WorldListener : Initializer {
+//$$    override fun init() {
+//$$        WorldRenderEvents.BLOCK_OUTLINE.register { _, ctx ->
+//$$            val event = CancellableEvent()
+//$$            TriggerType.RENDER_BLOCK_HIGHLIGHT.triggerAll(BlockPos(ctx.blockPos()), event)
+//$$            !event.isCancelled()
+//$$        }
+//$$        WorldRenderEvents.START.register { ctx ->
+//$$            val deltaTicks = ctx.tickCounter().dynamicDeltaTicks
+//$$            GUIRenderer.withMatrix(ctx.matrixStack(), deltaTicks) {
+//$$                TriggerType.PRE_RENDER_WORLD.triggerAll(deltaTicks)
+//$$            }
+//$$        }
+//$$        WorldRenderEvents.LAST.register { ctx ->
+//$$            val deltaTicks = ctx.tickCounter().dynamicDeltaTicks
+//$$            GUIRenderer.withMatrix(ctx.matrixStack(), deltaTicks) {
+//$$                TriggerType.POST_RENDER_WORLD.triggerAll(deltaTicks)
+//$$            }
+//$$        }
+//$$    }
+//$$}
+//#else
+import com.chattriggers.ctjs.MCBlockPos
+import net.minecraft.client.util.math.MatrixStack
+object WorldListener {
+    var matrixStack: MatrixStack? = null
+    private var deltaTicks: Float = 1f
+
+    fun triggerBlockOutline(bp: MCBlockPos): Boolean {
+        val event = CancellableEvent()
+        TriggerType.RENDER_BLOCK_HIGHLIGHT.triggerAll(BlockPos(bp), event)
+        return event.isCanceled()
+    }
+
+    fun triggerRenderStart(ticks: Float) {
+        deltaTicks = ticks
+        if (matrixStack == null) return
+        GUIRenderer.withMatrix(matrixStack, ticks) {
+            TriggerType.PRE_RENDER_WORLD.triggerAll(ticks)
+        }
+    }
+
+    fun triggerRenderLast() {
+        if (matrixStack == null) return
+        GUIRenderer.withMatrix(matrixStack, deltaTicks) {
+            TriggerType.POST_RENDER_WORLD.triggerAll(deltaTicks)
+        }
     }
 }
+//#endif